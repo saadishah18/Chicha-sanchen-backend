ac61dc58587dde39180fb3d8138a3161c9380d92
saad <s.siddiquishah@gmail.com>
2024-02-12 00:17:13 +0500

    fixing with product file

--- database/migrations/2023_12_20_200458_create_add_ons_table.php
+++ database/migrations/2023_12_20_200458_create_add_ons_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('add_ons', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('parent_id')->nullable();
             $table->string('name');
             $table->boolean('is_active')->default(1);
 //            $table->integer('price');
