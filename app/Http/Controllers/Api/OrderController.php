10037f33b9a4225514cee9e528d7b1df28137abf
saad <s.siddiquishah@gmail.com>
2024-07-01 00:28:42 +0500

    delete cart after order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -238,7 +238,7 @@ class OrderController extends Controller
                 $user = auth()->user();
                 $cart = Cart::where('user_id', $user->id)->first();
                 if($cart)
-//                    $cart->delete();
+                    $cart->delete();
                 // Set up the Stripe API key
                 Stripe::setApiKey(config('services.stripe.secret'));
 //                $paymentIntent = $result->createSetupIntent(['payment_method_types' => ['card']]);
