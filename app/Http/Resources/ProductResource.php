205f434859621b985828d1be2154cc85ca13bee2
saad <s.siddiquishah@gmail.com>
2024-07-03 00:36:06 +0500

    shorten product name

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,18 +16,41 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // Find the position of '+'
-        $plusPosition = strpos($this->name, '+');
+//        $name = $this->name; // Example: "Osmanthus Oolong Tea, Osmanthus Oolong Tea"
+//        $categoryName = $this->category->name; // Example: "Osmanthus Oolong Tea"
+//
+//        // Check if $categoryName is a prefix of $name
+//        if (strpos($name, $categoryName) === 0) {
+//            // Remove the first occurrence of $categoryName
+//            $trimmedName = trim(substr($name, strlen($categoryName)), ', ');
+//
+//            // If trimmedName is not empty, assign it back to $name
+//            if (!empty($trimmedName)) {
+//                $name = $trimmedName;
+//            }
+//        } else {
+//            // Remove the first two words from $name
+//            $words = explode(' + ', $name, 3);
+//            if (count($words) >= 2) {
+//                $name = isset($words[2]) ? $words[2] : $words[1];
+//            }
+//        }
+        $name = $this->name;
+// Split the name into words
+        $words = explode(' ', $name);
 
-// Slice the string to get the part after '+'
-        if ($plusPosition !== false) {
-            $name = trim(substr($this->name, $plusPosition + 1));
-        } else {
-            $name = $this->name; // If no '+' is found, return the original string
-        }
+// Remove the first word
+        array_shift($words);
+
+// Join the remaining words back into a string
+        $name = implode(' ', $words);
+
+// Update the original name
+        $this->name = $name;
+
         $result = [
             'id' => $this->id,
-            'name' => $name,
+            'name' => $this->name ,
             'description' => strip_tags($this->description),
             'category_id' => $this->category->id,
             'category' => $this->category->name,
