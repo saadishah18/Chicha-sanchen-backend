81a60c4136870c0f61dedefcca74aa121b030b75
saad <s.siddiquishah@gmail.com>
2024-07-11 15:57:23 +0500

    making stripe webhook

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\StripeWebhookEvent;
+use App\Events\StripeWebHookEventNew;
 use Illuminate\Http\Request;
 
 class GuestController extends Controller
@@ -10,9 +10,10 @@ class GuestController extends Controller
     public function webHook(Request $request)
     {
         $complete_object = $request->all();
+        dd($request->all());
 //        dispatch(new StripeWebhookEvent($complete_object));
 //        event(new StripeWebhookEvent($complete_object));
-        StripeWebhookEvent::dispatch($complete_object);
+        StripeWebHookEventNew::dispatch($complete_object);
         return response('success', 200);
     }
 
