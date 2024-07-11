2a3aa54430b21be703d86cbf437585ca6a8a417f
saad <s.siddiquishah@gmail.com>
2024-07-11 16:38:51 +0500

    making stripe webhook

--- routes/web.php
+++ routes/web.php
@@ -32,28 +32,28 @@ require __DIR__.'/auth.php';
 //});
 
 
-Route::post('stripe/web-hook', [\App\Http\Controllers\GuestController::class,'webHook'])->name('stripe.webhook');
+Route::post('stripe/web-hook', [\App\Http\Controllers\GuestController::class,'webHook']);
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
 //        Route::group(['prefix' => 'laravel-filemanager'], function () {
 //        \UniSharp\LaravelFilemanager\Lfm::routes();
 //    });
 //Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [\App\Http\Controllers\Admin\AdminDashboardController::class, 'dashboard'])->name('admin.dashboard');
 //    Route::get('/dashboard', [\App\Http\Controllers\Admin\AdminDashboardController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/profile/edit', [\App\Http\Controllers\Admin\AdminProfileController::class, 'edit'])->name('admin.profile.edit');
-    Route::patch('/profile', [\App\Http\Controllers\Admin\AdminProfileController::class, 'update'])->name('admin.profile.update');
-    Route::get('/profile/reset-password', [\App\Http\Controllers\Admin\AdminProfileController::class, 'resetPassword'])->name('admin.profile.reset_password');
+//    Route::get('/dashboard', [\App\Http\Controllers\Admin\AdminDashboardController::class, 'dashboard'])->name('admin.dashboard');
+//    Route::get('/profile/edit', [\App\Http\Controllers\Admin\AdminProfileController::class, 'edit'])->name('admin.profile.edit');
+//    Route::patch('/profile', [\App\Http\Controllers\Admin\AdminProfileController::class, 'update'])->name('admin.profile.update');
+//    Route::get('/profile/reset-password', [\App\Http\Controllers\Admin\AdminProfileController::class, 'resetPassword'])->name('admin.profile.reset_password');
 
     Route::prefix('users')->group(function () {
         Route::get('/', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('admin.users.index');
         Route::get('/datatable', [\App\Http\Controllers\Admin\UserController::class, 'datatable'])->name('admin.users.datatable');
-        Route::post('/', [\App\Http\Controllers\Admin\AdminUsersController::class, 'store'])->name('admin.users.store');
-        Route::get('/{id}/edit', [\App\Http\Controllers\Admin\AdminUsersController::class, 'edit'])->name('admin.users.edit');
-        Route::get('/create', [\App\Http\Controllers\Admin\AdminUsersController::class, 'create'])->name('admin.users.create');
-        Route::delete('/{id}', [\App\Http\Controllers\Admin\AdminUsersController::class, 'destroy'])->name('admin.users.destroy');
-        Route::post('/{id}/toggle-approve', [\App\Http\Controllers\Admin\AdminUsersController::class, 'toggleApprove'])->name('admin.users.toggle_approve');
-        Route::patch('/{id}/update', [\App\Http\Controllers\Admin\AdminUsersController::class, 'update'])->name('admin.users.update');
+//        Route::post('/', [\App\Http\Controllers\Admin\AdminUsersController::class, 'store'])->name('admin.users.store');
+//        Route::get('/{id}/edit', [\App\Http\Controllers\Admin\AdminUsersController::class, 'edit'])->name('admin.users.edit');
+//        Route::get('/create', [\App\Http\Controllers\Admin\AdminUsersController::class, 'create'])->name('admin.users.create');
+//        Route::delete('/{id}', [\App\Http\Controllers\Admin\AdminUsersController::class, 'destroy'])->name('admin.users.destroy');
+//        Route::post('/{id}/toggle-approve', [\App\Http\Controllers\Admin\AdminUsersController::class, 'toggleApprove'])->name('admin.users.toggle_approve');
+//        Route::patch('/{id}/update', [\App\Http\Controllers\Admin\AdminUsersController::class, 'update'])->name('admin.users.update');
     });
     Route::prefix('categories')->group(function () {
         Route::get('/', [\App\Http\Controllers\Admin\CategoryController::class, 'index'])->name('admin.categories.index');
